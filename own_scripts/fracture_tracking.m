424d3110fd214e40ba6aefe60b29df59ece1d39d
dr_njitram <martijn.gobes@gmail.com>
2020-03-24 16:50:46 +0100

    Added code to plot points on a frame and histograms of remainders

--- own_scripts/fracture_tracking.m
+++ own_scripts/fracture_tracking.m
@@ -1,2 +1,12 @@
 [image,map] = imread('data\colloids.tif', 1);
-imshow(image, [0, max(max(image))])
+[r] = feature2D(image,1,3,3000);
+
+figure
+imshow(image, [0, max(max(image))]);
+hold on
+plot(r(:,1), r(:,2), '.');
+
+figure
+histogram(mod(r(:,1), 1), 10)
+figure
+histogram(mod(r(:,2), 1), 10)
