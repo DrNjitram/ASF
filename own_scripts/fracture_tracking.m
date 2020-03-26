b8986f1371398e62b7572d6f061ff1ea725a4322
dr_njitram <martijn.gobes@gmail.com>
2020-03-26 16:50:03 +0100

    Add loading all images at once and perform tracking on them. Improved the checking for subpixel accuracy
    Also added a excel file that will allow us to check if the randomness standard deviation is random enough.

--- own_scripts/fracture_tracking.m
+++ own_scripts/fracture_tracking.m
@@ -1,12 +1,34 @@
-[image,map] = imread('data\colloids.tif', 1);
-[r] = feature2D(image,1,3,3000);
+clear 
+
+fname = 'data\colloids.tif';
+info = imfinfo(fname);
+num_images = numel(info);
+
+images = readtiffstack(fname);
+r = {num_images};
+
+for i = 1:num_images
+    disp(i)
+    r{i} = feature2D(images(:, :, i),1,3,3000);
+end
+
+
+no = 2;
 
 figure
-imshow(image, [0, max(max(image))]);
+imshow(images(:, :, no), [0, max(max(images(:, :, no)))]);
 hold on
-plot(r(:,1), r(:,2), '.');
+plot(r{2}(:,1), r{no}(:,2), '.');
+
+remainder_x = [];
+remainder_y = [];
+
+for i = 1:num_images
+    remainder_x = [remainder_x, std(mod(r{i}(:,1), 1)')];
+    remainder_y = [remainder_y, std(mod(r{i}(:,2), 1)')];
+end
 
 figure
-histogram(mod(r(:,1), 1), 10)
+histogram(remainder_x, 10)
 figure
-histogram(mod(r(:,2), 1), 10)
+histogram(remainder_y, 10)
