f5f48af87329a158858512cb58d9306f4e7f2c0b
dr_njitram <martijn.gobes@gmail.com>
2020-04-21 14:52:56 +0200

    Add calculating the strain area in meters rather than pixels

--- own_scripts/main.m
+++ own_scripts/main.m
@@ -3,11 +3,14 @@ close all
 
 %% Global variables
 frametime = 4.2; %s
+fov_resolution = 328*320; % surface area square um
+pixel_resolution = 934*912; %surface area square pixels
+surface_per_px = fov_resolution/pixel_resolution;
 FOV = 1; % Unknown 
 basepath = [pwd '\data\'];
 addpath([pwd '\Code_2D_feature_finding\']);
 start_frame = 20;
-end_frame = 50;
+end_frame = 35;
 numframes = end_frame - start_frame + 1;
 time = [0:numframes-1]*frametime;
 
@@ -22,7 +25,7 @@ barrg = 4;
 barcc = 0.5;
 IdivRg = barrI/barrg;
 maxdisp = 4; % def 1
-goodenough = numframes - 5;
+goodenough = numframes - 2;
 memory = 4; % def 2
 
 p_size = 20;
@@ -43,7 +46,7 @@ final = Strain_calc(dedrifted, p_size, false, 3);
 
 
 %% plotting
-frame_to_check = 45; % frame number
+frame_to_check = 30; % frame number
 frame_to_check = frame_to_check-start_frame+1; %real frame number
 
 %check_tracking(basepath, frame_to_check, featsize);
@@ -51,8 +54,8 @@ frame_to_check = frame_to_check-start_frame+1; %real frame number
 %truncate final array
 final_truncated = {numframes};
 for i = 1:numframes
-    %final_truncated{i} = final(final(:,2,i)>=300 & final(:,1,i) > 100 & final(:,1,i) < 500 ,:,i);
-    final_truncated{i} = final(final(:,2,i)>=300 ,:,i);
+    final_truncated{i} = final(final(:,2,i)>=300 & final(:,1,i) > 200 & final(:,1,i) < 500 ,:,i);
+    %final_truncated{i} = final(final(:,2,i)>=300 ,:,i);
 end
 
 h = figure;
@@ -77,21 +80,26 @@ end
 
 %% Plot process zone for frames of interest
 %final = final;  %the xx xy yy yx strain matrix
-t = frame_to_check;          %the frame you want to see
-strain_type = 8 ;%choose 3/4/5/6: 3 = xx; 4 = xy; 5 = yy 6 = yx
-strain_cut = 0.02; %choose from below which strain value you want to exclude particles
+t = 15;          %the frame you want to see
+strain_type = 9;%choose 3/4/5/6: 3 = xx; 4 = xy; 5 = yy 6 = yx, 8 all, 9 diagonals
+strain_cut = 0.010; %choose from below which strain value you want to exclude particles
 
-% [circle, a, P] = calc_processZone(final,frame_to_check,strain_type,strain_cut);
-% 
-% figure
-% plot(final_truncated(:,1,t),-1*final_truncated(:,2,t),'.');
-% hold on
-% plot(P(circle,1),-1*P(circle,2));
-% 
-% title("process Zone t="+t+", area="+a)
-% xlabel("X")
-% ylabel("Y")
-% legend("particle", "process zone")
+
+[circle, a, P] = calc_processZone(final_truncated{n},strain_type,strain_cut);
+areas(n) = a;
+figure
+scatter(final_truncated{t}(:,1), -1*final_truncated{t}(:,2), 20 , final_truncated{t}(:,strain_type),'filled');
+colorbar;
+caxis([-0.05, 0.05]);
+hold on
+plot(P(circle,1),-1*P(circle,2));
+
+title("process Zone t="+(start_frame+n)+", area="+(a*surface_per_px) + " um2")
+xlabel("X")
+ylabel("Y")
+legend("particle", "process zone")
+hold off
+
 
 areas = zeros(1, numframes);
 % create gif/video of all frames(if time allows)
@@ -106,14 +114,14 @@ for n = 1:numframes
 
     % plots the proces area at time t over all particles.
     [circle, a, P] = calc_processZone(final_truncated{n},strain_type,strain_cut);
-    areas(n) = a;
+    areas(n) = a * surface_per_px;
     scatter(final_truncated{n}(:,1), -1*final_truncated{n}(:,2), 20 , final_truncated{n}(:,strain_type),'filled');
     colorbar;
     caxis([-0.05, 0.05]);
     hold on
     plot(P(circle,1),-1*P(circle,2));
     
-    title("process Zone t="+(start_frame+n)+", area="+a)
+    title("process Zone t="+(start_frame+n)+", area="+(a*surface_per_px))
     xlabel("X")
     ylabel("Y")
     legend("particle", "process zone")
@@ -134,4 +142,7 @@ for n = 1:numframes
     end 
 end
 close(v);
+figure
 plot(start_frame+[1:numframes], areas);
+xlabel("time (frame)")
+ylabel("Area Strain")
